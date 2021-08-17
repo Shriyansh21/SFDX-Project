08f7440598d9575e29d8cdd30dc846b945529f41
Shriyansh Sharma <shriyanshsfdc21@gmail.com>
2021-08-17 16:14:51 +0530

    Components Commit

---
+++ force-app/main/default/lwc/testComponent/testComponent.js
@@ -0,0 +1,12 @@
+import { LightningElement } from 'lwc';
+
+export default class TestComponent extends LightningElement {
+
+newtestmethod()
+{
+    var newvar = 'This is JS file of the component';
+    console.log(newvar);
+
+}
+
+}
